176113fc56dd7d3b471c748adec4ef895ce912a6
Asritha Thota <thotasrilakshmiasritha@gmail.com>
2026-05-15 09:34:30 +0530

    custom server

--- Custom-server/server.js
+++ Custom-server/server.js
@@ -1,34 +1,55 @@
-const http=require('http');
-const os=require('os');
-const path=require('path');
-const EventEmitter=require('events');
+const http = require('http');
+const os = require('os');
+const path = require('path');
+const EventEmitter = require('events');
 
-const event=new EventEmitter();
+const event = new EventEmitter();
 
-const server=http.createServer((req,res)=>{
+const server = http.createServer((req, res) => {
+
     event.emit('requestMade');
-    res.writeHead(200,{'Content-Type':'text/plain'});
 
-    const osInfo=`
-    OS_INFO: ${os.type()}
-    OS_PLATFORM: ${os.platform()}
-    OS_CPU_ARCH: ${os.arch()}
-    OS_FREE_MEMORY: ${os.freemem()}
-    OS_TOTAL_MEMORY: ${os.totalmem()}
-    `;
+    if (req.url === '/') {
 
-    const file_path=path.join(__dirname,'server.js');
+        res.writeHead(200, { 'Content-Type': 'text/plain' });
 
-    res.end(
-        "Node JS CUSTOM SERVER\n\n"+
-        "OS INFORMATION\n"+osInfo+"\nPath Info\n"+"Working Directory: "+file_path
-    );
+        res.end("Home Page");
+
+    } 
+    else if (req.url === '/system') {
+
+        res.writeHead(200, { 'Content-Type': 'text/plain' });
+
+        const osInfo = `
+OS_INFO: ${os.type()}
+OS_PLATFORM: ${os.platform()}
+OS_CPU_ARCH: ${os.arch()}
+OS_FREE_MEMORY: ${os.freemem()}
+OS_TOTAL_MEMORY: ${os.totalmem()}
+`;
+
+        const file_path = path.join(__dirname, 'server.js');
+
+        res.end(
+            "SYSTEM INFORMATION\n\n" +
+            osInfo +
+            "\nPath Info\n" +
+            "Working Directory: " + file_path
+        );
+
+    } 
+    else {
+
+        res.writeHead(404, { 'Content-Type': 'text/plain' });
+
+        res.end("Page Not Found");
+    }
 });
 
-event.on('requestMade',()=>{
+event.on('requestMade', () => {
     console.log("Request Made to Server");
-})
+});
 
-server.listen(3000,()=>{
-    console.log("Server Started running");
-})
+server.listen(3000, () => {
+    console.log("Server running at http://localhost:3000");
+});
